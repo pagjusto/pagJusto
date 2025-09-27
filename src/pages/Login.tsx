d69067877fb2154adbe2f8ac9231bafe585ef0aa
[dyad] <git@dyad.sh>
2025-09-27 19:13:42 -0300

    [dyad] Aumentando mais o tamanho da logo - wrote 2 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,7 +9,7 @@ const Login: React.FC = () => {
             <img 
                 src="/logo.png"
                 alt="Pag Justo! Logo" 
-                className="w-56 sm:w-64 mx-auto mb-8"
+                className="w-72 sm:w-80 mx-auto mb-8"
             />
             <div className="w-full max-w-md">
                 <Auth
